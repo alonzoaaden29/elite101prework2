4673ea1f4a4cf646be4fddc07b1f0ad6fb35cb39
alonzoaaden29 <alonzoaaden@gmail.com>
2025-01-17 07:50:29 +0000

    menu

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 print("Welcome to the food ordering Chatbot")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you?")
+print("Awesome!, how may i help you?")
+
+def menu():
+    print("[1] Option 1")
+    print("[2] Option 2")
+    print("[0] Exit program")
+
+
+menu()
+option = int(input("Enter your option:"))
+
+while option !=0:
+    if option == 1:
+        print("Option 2 has been called.")
+    elif option == 2:
+        print("Option 2 has been called.")
+    else:
+        print("Invalid option.")
+
+        print()
+        menu()
+        option = int(input("Enter your option: "))
+
+
+print("Thanks for using this program. Adios!")
+
